4f46d9ed72212a9e694bb301ec0d3b3f7410ac2e
shuisheng <2274751790@qq.com>
2022-10-16 08:55:26 +0800

    feat: 修改脚本

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 # Date: 2022-05-10 11:51:50
 # Description:https://github.com/galaxy-s10/react18-blog-client
 # Email: 2274751790@qq.com
-# FilePath: /react18-blog-client/build.sh
+# FilePath: /github/react18-blog-client/build.sh
 # Github: https://github.com/galaxy-s10
-# LastEditTime: 2022-10-14 17:44:16
+# LastEditTime: 2022-10-16 08:52:29
 # LastEditors: shuisheng
 ###
 
@@ -24,33 +24,38 @@ PUBLICDIR=/node #约定公共目录为/node
 echo 删除node_modules:
 rm -rf node_modules
 
+echo 查看node版本:
+node -v
+
 echo 查看npm版本:
 npm -v
 
 echo 设置npm淘宝镜像:
-npm config set registry http://registry.npm.taobao.org/
+npm config set registry https://registry.npm.taobao.org/
 
 echo 查看当前npm镜像:
 npm get registry
 
-if ! type yarn >/dev/null 2>&1; then
-  echo 'yarn未安装,先全局安装yarn'
-  npm i yarn -g
+if ! type pnpm >/dev/null 2>&1; then
+  echo 'pnpm未安装,先全局安装pnpm'
+  npm i pnpm -g
 else
-  echo 'yarn已安装'
+  echo 'pnpm已安装'
 fi
 
-echo 查看yarn版本:
-yarn -v
+echo 查看pnpm版本:
+pnpm -v
 
-echo 设置yarn淘宝镜像:
-yarn config set registry https://registry.npm.taobao.org
+echo 设置pnpm淘宝镜像:
+pnpm config set registry https://registry.npm.taobao.org/
+pnpm config set @billd:registry http://registry.hsslive.cn/
 
-echo 查看当前yarn镜像:
-yarn config get registry
+echo 查看当前pnpm镜像:
+pnpm config get registry
+pnpm config set @billd:registry
 
 echo 开始安装依赖:
-yarn install
+pnpm install
 
 if [ $ENV = 'beta' ]; then
   echo 开始构建测试环境:
@@ -63,3 +68,8 @@ else
 fi
 
 npx cross-env REACT_APP_RELEASE_PROJECT_NAME=$JOBNAME REACT_APP_RELEASE_PROJECT_ENV=$ENV webpack --config ./script/config/webpack.common.ts --env production
+
+echo 清除buff/cache:
+
+sync
+echo 3 >/proc/sys/vm/drop_caches
